4744c2a19a9029059d52014f35d2fae3510adbfb
Solo <solomonarnett@gmail.com>
2018-01-11 17:11:37 -0500

    removed inset shadow from dashboard and hid some options

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,19 +12,21 @@ export class HomeComponent implements OnInit {
     // Registration Link
     "#registration",
     //Volunteer Link
-    "https://hackgsuspring2018.typeform.com/to/Jxrdks ",
+    "https://hackgsuspring2018.typeform.com/to/Jxrdks",
     // FAQ Link
     "#faq",
     // Travel Reimbursment Link
-    "https://hackgsuspring2018.typeform.com/to/EM26kg",
+    // "https://hackgsuspring2018.typeform.com/to/EM26kg",
+    "#"
   ];
 
   subGroupActions:Array<string> = [
     "mailto:hackgsu@gmail.com",
     // slack
-    "#slack",
+    "https://join.slack.com/t/hackgsuspring2018/shared_invite/enQtMjk3NjU4NDA3Njk4LTNjOWU5MDExZDkxMDIzZDQ3ZTYyYjA4NDY3ZjBmZWI2ZjI0OWNhYTlkYWJiNjY0MWFlNmI3MzZkZTAzZDdkMzM",
     // google form registration
-    "https://goo.gl/forms/wKNQA7wUcHkagtSQ2",
+    // "https://goo.gl/forms/wKNQA7wUcHkagtSQ2",
+    "#"
   ]
   constructor() { }
 
